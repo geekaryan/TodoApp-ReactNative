5384cb2f96b98ae9203c05fde7ed0fe75b3806f2
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-07-16 14:38:39 +0530

    done

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
-  TextInput,
-  Button,
   View,
   FlatList,
   Alert,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
+
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
-import Sandbox from "./components/Sandbox";
 
 export default function App() {
   const [todos, setTodos] = useState([
